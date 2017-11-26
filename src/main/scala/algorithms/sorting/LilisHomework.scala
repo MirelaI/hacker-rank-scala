4c3679316ef447ee45622e5ada06248f57b35c4d
MirelaI <imirela@cpan.org>
2017-11-26 19:24:13 +0000

    Add reverse to Lilis homework

--- src/main/scala/algorithms/sorting/LilisHomework.scala
+++ src/main/scala/algorithms/sorting/LilisHomework.scala
@@ -9,20 +9,19 @@ import scala.collection.mutable.ListBuffer
   */
 object LilisHomework {
 
-  def findNoSwaps(unsorted: ListBuffer[Int]): Int = {
-    val sorted: ListBuffer[Int] = unsorted.sorted
+  def findNoSwaps(unsorted: ListBuffer[Int], sorted: ListBuffer[Int]): Int = {
     var swap: Int = 0
 
     var indexes: Map[Int, Int] = unsorted.zipWithIndex.toMap
 
-    for ( i <- unsorted.indices ) {
+    for (i <- unsorted.indices) {
       if (unsorted(i) != sorted(i)) {
         swap += 1
 
         val indexToSwap = indexes(sorted(i))
         val elemToSwap = sorted(i)
         val temp = unsorted(i)
-        indexes += ( unsorted(i) -> indexes(elemToSwap))
+        indexes += (unsorted(i) -> indexes(elemToSwap))
 
         unsorted.update(i, sorted(i))
         unsorted.update(indexToSwap, temp)
@@ -38,8 +37,9 @@ object LilisHomework {
 
     val unsortedList: ListBuffer[Int] = sc.nextLine.split(" ").map{ _.toInt }.to[ListBuffer]
 
-    val noOfSwaps: Int = findNoSwaps(unsortedList)
+    val noOfSwaps: Int = findNoSwaps(unsortedList, unsortedList.sorted)
+    val noOfSwapsReversed: Int = findNoSwaps(unsortedList, unsortedList.sorted.reverse)
 
-    println(noOfSwaps)
+    println(List(noOfSwaps, noOfSwapsReversed).min)
   }
 }
